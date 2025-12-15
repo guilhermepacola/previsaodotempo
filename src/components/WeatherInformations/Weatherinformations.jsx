587c91b012f49716e2eaeb29aa4982830ce8f589
GuilhermePacola <gui.pfaccio@gmail.com>
2025-12-15 16:56:58 -0300

    Correção arquivos

--- src/components/WeatherInformations/Weatherinformations.jsx
+++ src/components/WeatherInformations/Weatherinformations.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import './Weatherinformations.css'
 
 function WeatherInformations({ weatherInfos }) {
@@ -28,37 +28,4 @@ function WeatherInformations({ weatherInfos }) {
     )
 }
 
-
-=======
-import './Weatherinformations.css'
-
-function WeatherInformations({ weatherInfos }) {
-
-        function convertDateTime(date) {
-        const newDate = new Date(date.dt * 1000).toLocaleDateString('pt-BR', { weekday: 'long'})
-        return newDate;
-    }
-
-    console.log(weatherInfos)
-    return (
-        <div className='weatherContainer'>
-            <h2>{weatherInfos.name}</h2>
-            <div className='weatherInfo'>
-                <img src={`http://openweathermap.org/img/wn/${weatherInfos.weather[0].icon}.png`} />
-                <p className='temperature'>{Math.round(weatherInfos.main.temp)}°C</p>
-            </div>
-
-            <p className='description'>{convertDateTime(weatherInfos)}, {weatherInfos.weather[0].description}</p>
-            
-            <div className='details'>
-                <p>Sensação térmica: {Math.round(weatherInfos.main.feels_like)}°C</p>
-                <p>Umidade:{weatherInfos.main.humidity}%</p>
-                <p>Pressão: {weatherInfos.main.pressure}</p>
-            </div>
-        </div>
-    )
-}
-
-
->>>>>>> 9a5c62f87f94b7038909be2a044b97ce4336adc5
 export default WeatherInformations
--- src/components/WeekWeatherInformations/WeekWeatherInformations.jsx
+++ src/components/WeekWeatherInformations/WeekWeatherInformations.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import './WeekWeatherInformations.css'
 
 function WeekWeatherInformations({ weekInfos }) {
@@ -48,57 +48,4 @@ function WeekWeatherInformations({ weekInfos }) {
     )
 }
 
-
-=======
-import './WeekWeatherInformations.css'
-
-function WeekWeatherInformations({ weekInfos }) {
-
-    console.log(weekInfos)
-    let dailyForecast = {}
-
-    for (let forecast of weekInfos.list) {
-        const date = new Date(forecast.dt * 1000).toLocaleDateString()
-
-        if (!dailyForecast[date]) {
-            dailyForecast[date] = forecast
-        }
-
-    }
-
-    const nextFiveDaysForecast = Object.values(dailyForecast).slice(1, 6)
-
-    function convertDateTime(date) {
-        const newDate = new Date(date.dt * 1000).toLocaleDateString('pt-BR', { weekday: 'long', day: '2-digit' })
-        return newDate;
-    }
-
-
-    return (
-        <div className='weather-container'>
-
-            <h3>Previsão da Semana</h3>
-            <div className="weather-card">
-                {nextFiveDaysForecast.map(forecast => (
-
-                    <div key={forecast.dt} className="weather-item">
-                        <p className="forecast-day">{convertDateTime(forecast)}</p>
-
-                        <img src={`http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`} alt="" />
-
-                        <p className="forecast-description">{forecast.weather[0].description}</p>
-
-                        <p className='forecast-temp'>Min {Math.round(forecast.main.temp_min)}°C / Máx {Math.round(forecast.main.temp_max)}°C</p>
-
-
-                    </div>
-                ))}
-            </div>
-
-        </div>
-    )
-}
-
-
->>>>>>> 9a5c62f87f94b7038909be2a044b97ce4336adc5
 export default WeekWeatherInformations
